f2493796fec78c5351fd952d639693141a039e59
Marcin Lewandowski <git_test_user@kie.ue.poznan.pl>
2014-05-25 14:53:30 +0200

    Dodana i przetestowana funkcja get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,4 +2,8 @@ def dodawanie():
     a= int(input('Wprowadz pierwsza liczbe'))
     b= int(input('Wprowadz druga liczbe'))
     print(a+b) 
+    
+def get_info():
+    print("To jest prosty kalkulator")
+get_info()
 dodawanie()
